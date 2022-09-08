b921e88cf40dd60dbdd72d2161b93aede4cd95a8
Vinicius Brandao <viniciusbrandao@vb4web.com>
2022-09-08 17:16:00 -0300

    Test for UserService class, create method with sucess.

--- apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
+++ apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -92,7 +93,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(users);
+
+        Users response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Users.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
