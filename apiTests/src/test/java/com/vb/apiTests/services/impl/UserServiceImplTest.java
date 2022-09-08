6642183224468bc2934509ed7a802767b5011f5f
Vinicius Brandao <viniciusbrandao@vb4web.com>
2022-09-08 19:01:17 -0300

    Test for UserService class, update method with sucess.

--- apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
+++ apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
@@ -122,7 +122,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(users);
+
+        Users response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Users.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
