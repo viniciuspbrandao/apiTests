05c1845666b405e2be50a9609310c0f2c4d2388a
Vinicius Brandao <viniciusbrandao@vb4web.com>
2022-09-09 10:29:58 -0300

    Test for UserService class, delete method with success.

--- apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
+++ apiTests/src/test/java/com/vb/apiTests/services/impl/UserServiceImplTest.java
@@ -21,7 +21,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -152,8 +152,11 @@ class UserServiceImplTest {
 
 
     @Test
-    void delete() {
-
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUsers);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUsers(){
